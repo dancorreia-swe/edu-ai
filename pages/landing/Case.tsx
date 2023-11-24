068bfbc17c49d36730661b486673fbe7328cdcc6
Pedro Rosa <devpedrorosa@gmail.com>
2023-11-24 00:59:48 -0300

    Fix: use of cases

--- pages/landing/Case.tsx
+++ pages/landing/Case.tsx
@@ -1,20 +1,44 @@
 import React from "react";
-
+import IconCard from "./IconCard";
+import { IconAdOff } from "@tabler/icons-react";
 const BoxSection = () => {
   return (
-    <div className="gap:6rem flex h-screen items-center justify-center  bg-slate-900">
-      <div className="grid max-w-4xl grid-cols-2 gap-8">
-        <div className="rounded-md bg-white p-16">
-          <p className="text-slate-900">USO DE CASO 1</p>
+    <div className="lg:px-4- relative mx-auto mt-32 max-w-[85rem] bg-slate-950 px-4 py-10 sm:px-2 lg:py-14 ">
+      <p className=" mb-10  text-center text-3xl font-extrabold text-white">
+        USO DE CASOS:
+      </p>
+      <div className="   mt-7   grid grid-cols-2 gap-20">
+        <div className="    w-auto items-center rounded-md bg-gradient-to-t from-slate-800/10 to-slate-900/90 p-20 shadow-lg shadow-blue-950/30">
+          {/* Icon Block */}
+          <IconCard
+            icon={IconAdOff}
+            title="Perguntas e Respostas"
+            description="Gere perguntas e respostas baseada no seu conteúdo e material de estudo"
+          />
         </div>
-        <div className="rounded-md bg-white p-16">
-          <p className="text-slate-900">USO DE CASO 2</p>
+        <div className="  bg   items-center  rounded-md bg-gradient-to-t from-slate-800/10 to-slate-900/90 p-20 shadow-lg shadow-blue-950/30">
+          {/* Icon Block */}
+          <IconCard
+            icon={IconAdOff}
+            title="Inteligência artificial"
+            description="Tenha a sua disposição um sistema de inteligência artificial de geração para gerar o conteúdo"
+          />
         </div>
-        <div className="rounded-md bg-white p-16">
-          <p className="text-slate-900">USO DE CASO 3</p>
+        <div className="w-auto items-center rounded-md bg-gradient-to-t from-slate-800/10 to-slate-900/90 p-20 shadow-lg shadow-blue-950/30">
+          {/* Icon Block */}
+          <IconCard
+            icon={IconAdOff}
+            title="Ajuste em tempo real"
+            description="Ajuste e reajuste os seus questionamentos e auxílios até o seu ponto ideal"
+          />
         </div>
-        <div className="rounded-md bg-white p-16">
-          <p className="text-slate-900">USO DE CASO 4</p>
+        <div className="shadow--900 w-auto items-center  rounded-md bg-gradient-to-t from-slate-800/10 to-slate-900/90 p-20 shadow-lg shadow-blue-950/30">
+          {/* Icon Block */}
+          <IconCard
+            icon={IconAdOff}
+            title="Perguntas e Respostas"
+            description="Gere perguntas e respostas baseada no seu conteúdo e material de estudo"
+          />
         </div>
       </div>
     </div>
