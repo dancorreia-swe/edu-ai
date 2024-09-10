8f08416dd4cdb1ce859a1fd51a17b935226f1d26
Daniel Correia <danielgalego.dev@gmail.com>
2024-09-10 11:00:05 -0300

    fix: link url

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,14 +3,18 @@
 import { trpc } from "@/app/_trpc/client";
 import { ThemeProvider } from "@/contexts/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/react-query";
+import { httpLink } from "@trpc/react-query";
 import { PropsWithChildren, useState } from "react";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+      links: [
+        httpLink({
+          url: "/api/trpc",
+        }),
+      ],
     }),
   );
 
